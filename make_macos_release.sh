c6e2e1f5430569cd940ba47a340b19d12bac5d8e
Michael Flaxman <mflaxman@gmail.com>
2020-11-06 14:00:03 -0600

    more cleanup

--- make_macos_release.sh
+++ make_macos_release.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/env bash
 
+# TODO: script must be run by system python, pyinstaller doesn't play nicely with pyenv :(
+
 # Verbose printing
 set -o xtrace
-
-# Hackey timer
-date
 
 # Abandon if anything errors
 set -e;
@@ -16,16 +15,18 @@ rm -rf build/
 rm -rf multiwallet.egg-info/
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 
-# Install virtualenv
+# Python stuff
 python3 --version
+# Install virtualenv (if not installed)
 python3 -m pip install virtualenv
+# Create virtualenv and install our software inside it
 python3 -m virtualenv .venv3
 source .venv3/bin/activate
+python3 -m pip install pyinstaller
 python3 setup.py install
 python3 -m pip freeze
 
-# Install pyinstaller (TODO: install this at system level outside of venv?)
-python3 -m pip install pyinstaller
+# Compile MacOs app
 pyinstaller multiwallet_gui/app.py --clean --windowed --name=multiwallet
 
 # Create DMG
@@ -40,4 +41,8 @@ create-dmg \
 cd ..
 
 # Hackey timer
-date
+# https://askubuntu.com/questions/1028924/how-do-i-use-seconds-inside-a-bash-script
+hrs=$(( SECONDS/3600 ))
+mins=$(( (SECONDS-hrs*3600)/60))
+secs=$(( SECONDS-hrs*3600-mins*60 ))
+printf 'Time spent: %02d:%02d:%02d\n' $hrs $mins $secs
