f85af68e73fdcce69393a01d7d9ce7a0b6f3cbfe
Michael Flaxman <mflaxman@gmail.com>
2020-11-06 23:38:11 -0600

    clean

--- make_macos_release.sh
+++ make_macos_release.sh
@@ -18,10 +18,12 @@ find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 # Python stuff
 python3 --version
 # Install virtualenv (if not installed)
+python3 -m pip uninstall virtualenv -y
 python3 -m pip install virtualenv
 # Create virtualenv and install our software inside it
 python3 -m virtualenv .venv3
 source .venv3/bin/activate
+python3 -m pip uninstall pyinstaller -y
 python3 -m pip install pyinstaller
 python3 setup.py install
 python3 -m pip freeze
