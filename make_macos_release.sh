1ba8071a696845846f62c4a166e10ac64cc52d0b
Michael Flaxman <mflaxman@gmail.com>
2020-11-09 16:05:12 -0600

    update script

--- make_macos_release.sh
+++ make_macos_release.sh
@@ -21,12 +21,13 @@ flake8 .
 # Python stuff
 python3 --version
 # Install virtualenv (if not installed)
-python3 -m pip uninstall virtualenv -y
+# python3 -m pip uninstall virtualenv -y
 python3 -m pip install virtualenv
 # Create virtualenv and install our software inside it
 python3 -m virtualenv .venv3
 source .venv3/bin/activate
-python3 -m pip uninstall pyinstaller -y
+# python3 -m pip uninstall pyinstaller -y
+python3 -m pip install -r requirements.txt
 python3 -m pip install pyinstaller
 python3 setup.py install
 python3 -m pip freeze
