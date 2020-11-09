b70dbdbc6e5e3bec279ad83f00fc1f1bafccc2f8
Michael Flaxman <mflaxman@gmail.com>
2020-11-09 11:37:31 -0600

    restructure

--- multiwallet_gui/app.py
+++ multiwallet_gui/app.py
@@ -1,46 +1,16 @@
-from PyQt5 import QtGui
 from PyQt5.QtWidgets import (
     QApplication,
     QDialog,
     QTabWidget,
-    QComboBox,
-    QCheckBox,
-    QGroupBox,
     QVBoxLayout,
     QWidget,
     QLabel,
     QLineEdit,
-    QPlainTextEdit,
-    QPushButton,
-    QDialogButtonBox,
-    QMessageBox,
 )
-from PyQt5.QtGui import QIcon
 
 import sys
 
-from buidl.hd import HDPrivateKey
-from buidl.mnemonic import WORD_LOOKUP, WORD_LIST
-
-
-def _get_all_valid_checksum_words(first_words):
-    to_return = []
-    for word in WORD_LIST:
-        try:
-            HDPrivateKey.from_mnemonic(first_words + " " + word)
-            to_return.append(word)
-        except KeyError as e:
-            # We have a word in first_words that is not in WORD_LIST
-            return [], "Invalid BIP39 Word: {}".format(e.args[0])
-        except ValueError:
-            pass
-
-    return to_return, ""
-
-
-def _clean_submisission(string):
-    # TODO: more advanced regex
-    return string.replace("  ", " ").strip()
+from seedpicker import SeedpickerTab
 
 
 class MultiwalletApp(QDialog):
@@ -52,7 +22,7 @@ class MultiwalletApp(QDialog):
         tabWidget = QTabWidget()
 
         for tab in (SeedpickerTab, ReceiveTab, SendTab):
-            tab_obj= tab()
+            tab_obj = tab()
             tabWidget.addTab(tab_obj, tab_obj.TITLE)
 
         vbox.addWidget(tabWidget)
@@ -60,136 +30,9 @@ class MultiwalletApp(QDialog):
         self.setLayout(vbox)
 
 
-def _msgbox_err(main_text=None, informative_text=None, detailed_text=None):
-        msg = QMessageBox()
-        # msg.setWindowTitle("foo")  # TODO: this doesn't work
-        if main_text:
-            msg.setText(main_text)
-        msg.setIcon(QMessageBox.Critical)
-        if informative_text:
-            msg.setInformativeText(informative_text)
-
-        if detailed_text:
-            msg.setDetailedText(detailed_text)
-        msg.exec_()
-
-
-class SeedpickerTab(QWidget):
-    TITLE = "Seedpicker"
-
-    def __init__(self):
-        super().__init__()
-        vbox = QVBoxLayout()
-
-        self.firstWordsLabel = QLabel("Enter first 23 words of your seed:")
-        # FIXME: pre-seeding for easier testing, get rid of this
-        self.firstWordsEdit = QPlainTextEdit("air air air air air air air air air air air air air air air air air air air air air air air")
-        self.firstWordsEdit.setPlaceholderText("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
-
-        self.firstWordsSubmitButton = QPushButton("Calculate Seed")
-        self.firstWordsSubmitButton.clicked.connect(self.process_submit)
-
-        self.privResultsLabel = QLabel("")
-        self.privResultsEdit = QPlainTextEdit("")
-        self.privResultsEdit.setReadOnly(True)
-        self.privResultsEdit.setHidden(True)
-
-        self.pubResultsLabel = QLabel("")
-        self.pubResultsEdit = QPlainTextEdit("")
-        self.pubResultsEdit.setReadOnly(True)
-        self.pubResultsEdit.setHidden(True)
-
-        vbox.addWidget(self.firstWordsLabel)
-        vbox.addWidget(self.firstWordsEdit)
-        vbox.addWidget(self.firstWordsSubmitButton)
-        vbox.addWidget(self.privResultsLabel)
-        vbox.addWidget(self.privResultsEdit)
-        vbox.addWidget(self.pubResultsLabel)
-        vbox.addWidget(self.pubResultsEdit)
-
-        self.setLayout(vbox)
-
-    def process_submit(self):
-        # Clear any previous submission in case of errors
-        self.privResultsEdit.clear()
-        self.privResultsEdit.setHidden(True)
-        self.privResultsLabel.setText("")
-        self.pubResultsEdit.clear()
-        self.pubResultsEdit.setHidden(True)
-        self.pubResultsLabel.setText("")
-        # TODO: why setText and not hide? # FIXME
-
-        first_words = _clean_submisission(self.firstWordsEdit.toPlainText())
-        fw_num = len(first_words.split())
-        if fw_num not in (11, 14, 17, 20, 23):
-            # TODO: 11, 14, 17, or 20 word seed phrases also work but this is not documented as it's for advanced users
-            return _msgbox_err(
-                    main_text="Invalid seed phrase",
-                    informative_text="Seed phrase must be 23 words",
-                    detailed_text=f"you entered {fw_num} words",
-                    )
-
-        wordlist_errors = []
-        for cnt, word in enumerate(first_words.split()):
-            if word not in WORD_LOOKUP:
-                wordlist_errors.append([cnt + 1, word])
-        if wordlist_errors:
-            # self.text.config(fg='red') (need a UI to turn this off on typing)
-            detailed_text = [
-                "The following are not valid:",
-            ]
-            detailed_text.extend([f"  word #{x[0]} {x[1]}" for x in wordlist_errors])
-            return _msgbox_err(
-                main_text="Non BIP39 words",
-                informative_text="Your seed phrase can ONLY contain BIP39 words",
-                detailed_text="\n".join(detailed_text),
-            )
-
-        valid_checksum_words, err_str = _get_all_valid_checksum_words(first_words)
-        if err_str:
-
-            return _msgbox_err(
-                    main_text="Error calculating checksum word",
-                    informative_text=err_str,
-                    )
-
-        IS_TESTNET = True  # TESTNET ONLY FOR NOW
-        if IS_TESTNET:
-            PATH = "m/48'/1'/0'/2'"
-            SLIP132_VERSION_BYTES = "02575483"
-        else:
-            PATH = "m/48'/0'/0'/2'"
-            SLIP132_VERSION_BYTES = "02aa7ed3"
-
-        last_word = valid_checksum_words[0]
-        hd_priv = HDPrivateKey.from_mnemonic(first_words + " " + last_word)
-
-        priv_to_display = [
-            f"Last Word: {last_word}",
-            f"Full {fw_num + 1} word mnemonic (including last word): {first_words + ' ' + last_word}",
-        ]
-        pub_to_display = [
-            "[{}{}]{}".format(
-                hd_priv.fingerprint().hex(),
-                PATH.replace("m", "").replace("'", "h"),
-                hd_priv.traverse(PATH).xpub(
-                    version=bytes.fromhex(SLIP132_VERSION_BYTES)
-                ),
-            ),
-        ]
-
-        self.privResultsLabel.setText("SECRET INFO - guard this VERY carefully")
-        self.privResultsEdit.setHidden(False)
-        self.privResultsEdit.appendPlainText("\n".join(priv_to_display))
-
-        self.pubResultsLabel.setText(f"PUBLIC KEY INFO ({'testnet' if IS_TESTNET else 'mainnet'}) - Copy-paste this into Specter-Desktop")
-        self.pubResultsEdit.setHidden(False)
-        self.pubResultsEdit.appendPlainText("\n".join(pub_to_display))
-
-
 class ReceiveTab(QWidget):
     TITLE = "Receive"
-    
+
     def __init__(self, parent=None):
         super(ReceiveTab, self).__init__(parent)
 
--- multiwallet_gui/seedpicker.py
+++ multiwallet_gui/seedpicker.py
@@ -1,11 +1,18 @@
+from helper import _clean_submisission, _msgbox_err
+from PyQt5.QtWidgets import (
+    QVBoxLayout,
+    QWidget,
+    QLabel,
+    QPlainTextEdit,
+    QPushButton,
+)
+
 from buidl.hd import HDPrivateKey
 from buidl.mnemonic import WORD_LOOKUP, WORD_LIST
 
-import tkinter as tk
-from tkinter import messagebox  # https://stackoverflow.com/a/29780454/1754586
-
 
 def _get_all_valid_checksum_words(first_words):
+    # TODO: move to buidl library
     to_return = []
     for word in WORD_LIST:
         try:
@@ -20,43 +27,64 @@ def _get_all_valid_checksum_words(first_words):
     return to_return, ""
 
 
-class SeedpickerFrame(tk.Frame):
-    TAB_NAME = "Seedpicker"
+class SeedpickerTab(QWidget):
+    TITLE = "Seedpicker"
 
-    def __init__(self, parent):
-        # TODO: WET
-        self.parent = parent
-        self.frame = tk.Frame.__init__(self, parent)
+    def __init__(self):
+        super().__init__()
+        vbox = QVBoxLayout()
 
-        label = tk.Label(self, text="Enter first 23 words of your seed:")
-        label.grid(column=0, row=1)
-
-        # https://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html
-        self.text = tk.Text(self, height=5)
-        self.text.grid()
-
-        seedpicker_submit_btn = tk.Button(
-            self, text="Calculate Last Word", command=self.first_words_validation
+        self.firstWordsLabel = QLabel("Enter first 23 words of your seed:")
+        # FIXME: pre-seeding for easier testing, get rid of this
+        self.firstWordsEdit = QPlainTextEdit(
+            "air air air air air air air air air air air air air air air air air air air air air air air"
         )
-        seedpicker_submit_btn.grid()
+        self.firstWordsEdit.setPlaceholderText(
+            "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo"
+        )
 
-        self.result = tk.Text(self, height=15)
-        self.result.grid_forget()
+        self.firstWordsSubmitButton = QPushButton("Calculate Seed")
+        self.firstWordsSubmitButton.clicked.connect(self.process_submit)
 
-    def first_words_validation(self):
-        # delete whatever text might have been in the results box
-        self.result.delete(1.0, tk.END)
+        self.privResultsLabel = QLabel("")
+        self.privResultsEdit = QPlainTextEdit("")
+        self.privResultsEdit.setReadOnly(True)
+        self.privResultsEdit.setHidden(True)
 
-        first_words = self.text.get("1.0", tk.END).replace("  ", " ").strip()
-        if not first_words:
-            return
+        self.pubResultsLabel = QLabel("")
+        self.pubResultsEdit = QPlainTextEdit("")
+        self.pubResultsEdit.setReadOnly(True)
+        self.pubResultsEdit.setHidden(True)
 
+        vbox.addWidget(self.firstWordsLabel)
+        vbox.addWidget(self.firstWordsEdit)
+        vbox.addWidget(self.firstWordsSubmitButton)
+        vbox.addWidget(self.privResultsLabel)
+        vbox.addWidget(self.privResultsEdit)
+        vbox.addWidget(self.pubResultsLabel)
+        vbox.addWidget(self.pubResultsEdit)
+
+        self.setLayout(vbox)
+
+    def process_submit(self):
+        # Clear any previous submission in case of errors
+        self.privResultsEdit.clear()
+        self.privResultsEdit.setHidden(True)
+        self.privResultsLabel.setText("")
+        self.pubResultsEdit.clear()
+        self.pubResultsEdit.setHidden(True)
+        self.pubResultsLabel.setText("")
+        # TODO: why setText and not hide? # FIXME
+
+        first_words = _clean_submisission(self.firstWordsEdit.toPlainText())
         fw_num = len(first_words.split())
         if fw_num not in (11, 14, 17, 20, 23):
             # TODO: 11, 14, 17, or 20 word seed phrases also work but this is not documented as it's for advanced users
-            err = f"Enter 23 word seed-phrase (you entered {fw_num} words)"
-            messagebox.showinfo(message=err)
-            return
+            return _msgbox_err(
+                main_text="Invalid seed phrase",
+                informative_text="Seed phrase must be 23 words",
+                detailed_text=f"you entered {fw_num} words",
+            )
 
         wordlist_errors = []
         for cnt, word in enumerate(first_words.split()):
@@ -64,19 +92,23 @@ class SeedpickerFrame(tk.Frame):
                 wordlist_errors.append([cnt + 1, word])
         if wordlist_errors:
             # self.text.config(fg='red') (need a UI to turn this off on typing)
-            msg = [
+            detailed_text = [
                 "The following are not valid:",
             ]
-            msg.extend([f"  word #{x[0]} {x[1]}" for x in wordlist_errors])
-            messagebox.showinfo(message="\n".join(msg))
-            return
-
-        # self.text.config(fg='black')
+            detailed_text.extend([f"  word #{x[0]} {x[1]}" for x in wordlist_errors])
+            return _msgbox_err(
+                main_text="Non BIP39 words",
+                informative_text="Your seed phrase can ONLY contain BIP39 words",
+                detailed_text="\n".join(detailed_text),
+            )
 
         valid_checksum_words, err_str = _get_all_valid_checksum_words(first_words)
         if err_str:
-            messagebox.showinfo(f"Error calculating checksum word: {err_str}")
-            return
+
+            return _msgbox_err(
+                main_text="Error calculating checksum word",
+                informative_text=err_str,
+            )
 
         IS_TESTNET = True  # TESTNET ONLY FOR NOW
         if IS_TESTNET:
@@ -89,15 +121,12 @@ class SeedpickerFrame(tk.Frame):
         last_word = valid_checksum_words[0]
         hd_priv = HDPrivateKey.from_mnemonic(first_words + " " + last_word)
 
-        to_display = [
-            "SECRET INFO - guard this VERY carefully",
+        priv_to_display = [
             f"Last Word: {last_word}",
             f"Full {fw_num + 1} word mnemonic (including last word): {first_words + ' ' + last_word}",
-            "",
-            f"PUBLIC KEY INFO ({'testnet' if IS_TESTNET else 'mainnet'})",
-            "Copy-paste this into Specter-Desktop:",
-            "",
-            "  [{}{}]{}".format(
+        ]
+        pub_to_display = [
+            "[{}{}]{}".format(
                 hd_priv.fingerprint().hex(),
                 PATH.replace("m", "").replace("'", "h"),
                 hd_priv.traverse(PATH).xpub(
@@ -106,6 +135,12 @@ class SeedpickerFrame(tk.Frame):
             ),
         ]
 
-        self.result.insert(tk.END, "\n".join(to_display))
-        # TODO: disable editing in a way that still allows copy-paste
-        self.result.grid()
+        self.privResultsLabel.setText("SECRET INFO - guard this VERY carefully")
+        self.privResultsEdit.setHidden(False)
+        self.privResultsEdit.appendPlainText("\n".join(priv_to_display))
+
+        self.pubResultsLabel.setText(
+            f"PUBLIC KEY INFO ({'testnet' if IS_TESTNET else 'mainnet'}) - Copy-paste this into Specter-Desktop"
+        )
+        self.pubResultsEdit.setHidden(False)
+        self.pubResultsEdit.appendPlainText("\n".join(pub_to_display))
