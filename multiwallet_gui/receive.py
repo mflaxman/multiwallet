ab2ee7968875642547c3329431f5ec5a7e284626
Michael Flaxman <mflaxman@gmail.com>
2020-11-09 11:58:18 -0600

    stub

--- multiwallet_gui/receive.py
+++ multiwallet_gui/receive.py
@@ -8,6 +8,11 @@ from PyQt5.QtWidgets import (
 )
 
 
+TEST_PSBT = """
+{ "label": "Any Recovery", "blockheight": 1863986, "descriptor": "wsh(sortedmulti(1,[c7d0648a\/48h\/1h\/0h\/2h]tpubDEpefcgzY6ZyEV2uF4xcW2z8bZ3DNeWx9h2BcwcX973BHrmkQxJhpAXoSWZeHkmkiTtnUjfERsTDTVCcifW6po3PFR1JRjUUTJHvPpDqJhr\/0\/*,[12980eed\/48h\/1h\/0h\/2h]tpubDEkXGoQhYLFnYyzUGadtceUKbzVfXVorJEdo7c6VKJLHrULhpSVLC7fo89DDhjHmPvvNyrun2LTWH6FYmHh5VaQYPLEqLviVQKh45ufz8Ae\/0\/*,[3a52b5cd\/48h\/1h\/0h\/2h]tpubDFdbVee2Zna6eL9TkYBZDJVJ3RxGYWgChksXBRgw6y6PU1jWPTXUqag3CBMd6VDwok1hn5HZGvg6ujsTLXykrS3DwbxqCzEvWoT49gRJy7s\/0\/*,[f7d04090\/48h\/1h\/0h\/2h]tpubDF7FTuPECTePubPXNK73TYCzV3nRWaJnRwTXD28kh6Fz4LcaRzWwNtX153J7WeJFcQB2T6k9THd424Kmjs8Ps1FC1Xb81TXTxxbGZrLqQNp\/0\/*))#tatkmj5q" } 
+""".strip()
+
+
 class ReceiveTab(QWidget):
     TITLE = "Receive"
 
@@ -17,12 +22,8 @@ class ReceiveTab(QWidget):
 
         self.descriptorLabel = QLabel("Desciptor")
         # FIXME: pre-seeding for easier testing, get rid of this
-        self.descriptorEdit = QPlainTextEdit(
-            "air air air air air air air air air air air air air air air air air air air air air air air"
-        )
-        self.descriptorEdit.setPlaceholderText(
-            "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo"
-        )
+        self.descriptorEdit = QPlainTextEdit(TEST_PSBT)
+        self.descriptorEdit.setPlaceholderText("wsh(sortedmulti(2,...))#...")
 
         self.descriptorSubmitButton = QPushButton("Derive Addresses")
         self.descriptorSubmitButton.clicked.connect(self.process_submit)
